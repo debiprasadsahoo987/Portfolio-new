ee943fcf06cc080ecf9e6e2b9b802c9bdfa9b7bb
Debiprasad <debiprasadsahoo987@gmail.com>
2024-09-20 12:04:59 +0530

    20th changes - navbar and hero(partial)

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,48 +1,57 @@
 /* eslint-disable react/no-unknown-property */
 import { Leva, useControls } from "leva";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import WorkSpace from "../components/WorkSpace";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader";
+import { calculateSizes } from "../constants/index.js";
+import { useMediaQuery } from "react-responsive";
 const Hero = () => {
-  const x = useControls("WorkSpace", {
-    positionX: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionY: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    positionZ: {
-      value: 2.5,
-      min: -10,
-      max: 10,
-    },
-    rotationX: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationY: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    rotationZ: {
-      value: 0,
-      min: -10,
-      max: 10,
-    },
-    scale: {
-      value: 1,
-      min: 0.1,
-      max: 10,
-    },
-  });
+  //   const x = useControls("WorkSpace", {
+  //     positionX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionY: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     positionZ: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationX: {
+  //       value: 0,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationY: {
+  //       value: 0,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     rotationZ: {
+  //       value: 0,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //     scale: {
+  //       value: 1,
+  //       min: 0.1,
+  //       max: 10,
+  //     },
+  //   });
+
+  const isSmall = useMediaQuery({ maxWidth: 660 });
+  const isMobile = useMediaQuery({ maxWidth: 938 });
+  const isTablet = useMediaQuery({ maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-40 mt-40 c-space gap-3">
@@ -53,14 +62,17 @@ const Hero = () => {
       </div>
 
       <div className="w-full h-full absolute inset-0">
-        <Leva />
+        <Leva hidden />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <WorkSpace
-              position={[x.positionX, x.positionY, x.positionZ]}
-              rotation={[x.rotationX, x.rotationY, x.rotationZ]}
-              scale={[x.scale, x.scale, x.scale]}
+              //   position={[x.positionX, x.positionY, x.positionZ]}
+              //   rotation={[x.rotationX, x.rotationY, x.rotationZ]}
+              //   scale={[x.scale, x.scale, x.scale]}
+              position={sizes.deskPosition}
+              rotation={sizes.deskRotation}
+              scale={sizes.deskScale}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
